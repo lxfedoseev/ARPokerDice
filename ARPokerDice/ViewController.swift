458de73634a965b9c7e25dba8b5ceeca38c4a531
Alex Fedoseev <lxfedoseev@gmail.com>
2019-01-24 21:20:58 +0300

    AR chapter 5

--- ARPokerDice/ViewController.swift
+++ ARPokerDice/ViewController.swift
@@ -32,81 +32,94 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController {
+// MARK: - Game State
+enum GameState: Int16 {
+    case detectSurface
+    case pointToSurface
+    case swipeToPlay
+}
 
+class ViewController: UIViewController {
+  
   // MARK: - Properties
   var trackingStatus: String = ""
   var focusNode: SCNNode!
   var diceNodes: [SCNNode] = []
-  var diceCount: Int = 1
+  var diceCount: Int = 5
   var diceStyle: Int = 0
   var diceOffset: [SCNVector3] = [SCNVector3(0.0,0.0,0.0),
                                   SCNVector3(-0.05, 0.00, 0.0),
                                   SCNVector3(0.05, 0.00, 0.0),
                                   SCNVector3(-0.05, 0.05, 0.02),
                                   SCNVector3(0.05, 0.05, 0.02)]
-
+    var gameState: GameState = .detectSurface
+    var statusMessage: String = ""
+    var focusPoint:CGPoint!
+    
   // MARK: - Outlets
-
+  
   @IBOutlet var sceneView: ARSCNView!
   @IBOutlet weak var statusLabel: UILabel!
   @IBOutlet weak var startButton: UIButton!
   @IBOutlet weak var styleButton: UIButton!
   @IBOutlet weak var resetButton: UIButton!
-
+  
   // MARK: - Actions
-
+  
   @IBAction func startButtonPressed(_ sender: Any) {
   }
-
+  
   @IBAction func styleButtonPressed(_ sender: Any) {
     diceStyle = diceStyle >= 4 ? 0 : diceStyle + 1
   }
-
+  
   @IBAction func resetButtonPressed(_ sender: Any) {
   }
-
+  
   @IBAction func swipeUpGestureHandler(_ sender: Any) {
-    // 1
-    guard let frame = self.sceneView.session.currentFrame else { return }
-    // 2
+    guard let frame = sceneView.session.currentFrame else { return }
     for count in 0..<diceCount {
       throwDiceNode(transform: SCNMatrix4(frame.camera.transform),
                     offset: diceOffset[count])
     }
   }
-
+  
   // MARK: - View Management
-
+  
+    @objc func orientationChanged() {
+        focusPoint = CGPoint(x: view.center.x,
+                             y: view.center.y + view.center.y * 0.25)
+    }
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.initSceneView()
-    self.initScene()
-    self.initARSession()
-    self.loadModels()
+    initSceneView()
+    initScene()
+    initARSession()
+    loadModels()
   }
-
+  
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     print("*** ViewWillAppear()")
   }
-
+  
   override func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
     print("*** ViewWillDisappear()")
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     print("*** DidReceiveMemoryWarning()")
   }
-
+  
   override var prefersStatusBarHidden: Bool {
     return true
   }
-
+  
   // MARK: - Initialization
-
+  
   func initSceneView() {
     sceneView.delegate = self
     sceneView.showsStatistics = true
@@ -116,8 +129,14 @@ class ViewController: UIViewController {
       //SCNDebugOptions.showBoundingBoxes,
       //SCNDebugOptions.showWireframe
     ]
+    focusPoint = CGPoint(x: view.center.x,
+                         y: view.center.y + view.center.y * 0.25)
+    
+    NotificationCenter.default.addObserver(self, selector: #selector(ViewController.orientationChanged),
+                                           name: UIDevice.orientationDidChangeNotification,
+                                           object: nil)
   }
-
+  
   func initScene() {
     let scene = SCNScene()
     scene.isPaused = false
@@ -125,21 +144,22 @@ class ViewController: UIViewController {
     scene.lightingEnvironment.contents = "PokerDice.scnassets/Textures/Environment_CUBE.jpg"
     scene.lightingEnvironment.intensity = 2
   }
-
+  
   func initARSession() {
     guard ARWorldTrackingConfiguration.isSupported else {
       print("*** ARConfig: AR World Tracking Not Supported")
       return
     }
-
+    
     let config = ARWorldTrackingConfiguration()
     config.worldAlignment = .gravity
     config.providesAudioData = false
+    config.planeDetection = .horizontal
     sceneView.session.run(config)
   }
-
+  
   // MARK: - Load Models
-
+  
   func loadModels() {
     // 1
     let diceScene = SCNScene(
@@ -151,93 +171,188 @@ class ViewController: UIViewController {
         withName: "dice\(count)",
         recursively: false)!)
     }
-
+    
     let focusScene = SCNScene(
       named: "PokerDice.scnassets/Models/FocusScene.scn")!
     focusNode = focusScene.rootNode.childNode(
       withName: "focus", recursively: false)!
-
+    
     sceneView.scene.rootNode.addChildNode(focusNode)
   }
-
+  
   // MARK: - Helper Functions
-
+    
+    func removeARPlaneNode(node: SCNNode){
+        for childNode in node.childNodes {
+            childNode.removeFromParentNode()
+        }
+    }
+    
+    func updateFocusNode() {
+        // 1
+        let results = self.sceneView.hitTest(self.focusPoint,
+                                             types: [.existingPlaneUsingExtent])
+        // 2
+        if results.count == 1 {
+            if let match = results.first {
+                // 3
+                let t = match.worldTransform
+                // 4
+                self.focusNode.position = SCNVector3( x: t.columns.3.x,
+                                                      y: t.columns.3.y,
+                                                      z: t.columns.3.z)
+                self.gameState = .swipeToPlay
+            }
+        } else { // 5
+            self.gameState = .pointToSurface
+        }
+    }
+    
+    func updateARPlaneNode(
+        planeNode: SCNNode, planeAchor: ARPlaneAnchor) {
+        let planeGeometry = planeNode.geometry as! SCNPlane
+        planeGeometry.width = CGFloat(planeAchor.extent.x)
+        planeGeometry.height = CGFloat(planeAchor.extent.z)
+        planeNode.position = SCNVector3Make(planeAchor.center.x, 0,
+                                            planeAchor.center.z)
+        
+    }
+  
   func throwDiceNode(transform: SCNMatrix4, offset: SCNVector3) {
-    // 1
     let position = SCNVector3(transform.m41 + offset.x,
                               transform.m42 + offset.y,
                               transform.m43 + offset.z)
-    // 2
     let diceNode = diceNodes[diceStyle].clone()
     diceNode.name = "dice"
     diceNode.position = position
-    //3
     sceneView.scene.rootNode.addChildNode(diceNode)
-    //diceCount -= 1
+    diceCount -= 1
   }
+    
+    func createARPlaneNode(planeAnchor: ARPlaneAnchor, color: UIColor) -> SCNNode {
+        let planeGeometry = SCNPlane(
+            width: CGFloat(planeAnchor.extent.x),
+            height: CGFloat(planeAnchor.extent.z))
+        let planeMaterial = SCNMaterial()
+        planeMaterial.diffuse.contents =
+        "PokerDice.scnassets/Textures/Surface_DIFFUSE.png"
+        planeGeometry.materials = [planeMaterial]
+        // 1 - Create plane node
+        let planeNode = SCNNode(geometry: planeGeometry)
+        // 2
+        planeNode.position = SCNVector3Make(
+            planeAnchor.center.x, 0, planeAnchor.center.z)
+        // 3
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+        // 4
+        return planeNode
+    }
+
 }
 
+
 extension ViewController : ARSCNViewDelegate {
-
+  
   // MARK: - SceneKit Management
-
+  
   func renderer(_ renderer: SCNSceneRenderer,
                 updateAtTime time: TimeInterval) {
     DispatchQueue.main.async {
-      self.statusLabel.text = self.trackingStatus
+      //self.statusLabel.text = self.trackingStatus
+        self.updateStatus()
+        self.updateFocusNode()
     }
   }
-
-
+  
+    func updateStatus() {
+        // 1
+        switch gameState {
+        case .detectSurface:
+            statusMessage = "Scan entire table surface...\nHit START when ready!"
+        case .pointToSurface:
+            statusMessage = "Point at designated surface first!"
+        case .swipeToPlay:
+            statusMessage = "Swipe UP to throw!\nTap on dice to collect it again."
+        }
+        // 2
+        self.statusLabel.text = trackingStatus != "" ?
+        "\(trackingStatus)" : "\(statusMessage)"
+    }
+  
   // MARK: - Session State Management
-
-  func session(_ session: ARSession,
-               cameraDidChangeTrackingState camera: ARCamera) {
+  
+  func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
     switch camera.trackingState {
-    // 1
     case .notAvailable:
-      self.trackingStatus = "Tacking:  Not available!"
-      break
-    // 2
+      trackingStatus = "Tacking:  Not available!"
     case .normal:
-      self.trackingStatus = "Tracking: All Good!"
-      break
-    // 3
+      trackingStatus = "Tracking: All Good!"
     case .limited(let reason):
       switch reason {
       case .excessiveMotion:
-        self.trackingStatus = "Tracking: Limited due to excessive motion!"
-        break
-      // 3.1
+        trackingStatus = "Tracking: Limited due to excessive motion!"
       case .insufficientFeatures:
-        self.trackingStatus = "Tracking: Limited due to insufficient features!"
-        break
-      // 3.2
+        trackingStatus = "Tracking: Limited due to insufficient features!"
       case .initializing:
-        self.trackingStatus = "Tracking: Initializing..."
-        break
+        trackingStatus = "Tracking: Initializing..."
       case .relocalizing:
-        self.trackingStatus = "Tracking: Relocalizing..."
+        trackingStatus = "Tracking: Relocalizing..."
       }
     }
   }
-
-  // MARK: - Session Error Managent
-
+  
+  // MARK: - Session Error Management
+  
   func session(_ session: ARSession,
                didFailWithError error: Error) {
-    self.trackingStatus = "AR Session Failure: \(error)"
+    trackingStatus = "AR Session Failure: \(error)"
   }
-
+  
   func sessionWasInterrupted(_ session: ARSession) {
-    self.trackingStatus = "AR Session Was Interrupted!"
+    trackingStatus = "AR Session Was Interrupted!"
   }
-
+  
   func sessionInterruptionEnded(_ session: ARSession) {
-    self.trackingStatus = "AR Session Interruption Ended"
+    trackingStatus = "AR Session Interruption Ended"
   }
-
+  
   // MARK: - Plane Management
-
+    
+    // 1
+    func renderer(_ renderer: SCNSceneRenderer,
+                  didUpdate node: SCNNode, for anchor: ARAnchor) {
+        // 2
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        // 3
+        DispatchQueue.main.async {
+            // 4
+            self.updateARPlaneNode(planeNode: node.childNodes[0],
+                                   planeAchor: planeAnchor)
+        }
+    }
+    
+    // 1
+    func renderer(_ renderer: SCNSceneRenderer,
+                  didAdd node: SCNNode, for anchor: ARAnchor) {
+        // 2
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        // 3
+        DispatchQueue.main.async {
+            // 4
+            let planeNode = self.createARPlaneNode(
+                planeAnchor: planeAnchor,
+                color: UIColor.yellow.withAlphaComponent(0.5))
+            // 5
+            node.addChildNode(planeNode)
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else { return }
+        
+        DispatchQueue.main.async {
+            self.removeARPlaneNode(node: node)
+        }
+    }
+    
 }
-
